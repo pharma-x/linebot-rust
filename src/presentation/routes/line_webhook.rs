0cad29a9281036dce1f181b2e9b42f889ebf62d0
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-05-23 17:10:02 +0900

    presentation層を修正した

--- src/presentation/routes/line_webhook.rs
+++ src/presentation/routes/line_webhook.rs
@@ -1,14 +1,20 @@
+use crate::adapter::repository::line_user_auth::LineUserAuthRepository;
+use crate::application::usecase::linebot_webhook_usecase::LinebotWebhookUseCase;
 use crate::presentation::model::line_webhook::{LineWebhookEventType, LineWebhookRequest};
 use axum::{http::StatusCode, response::IntoResponse, Json};
+use std::sync::Arc;
 
 pub async fn handler(Json(payload): Json<LineWebhookRequest>) -> impl IntoResponse {
-    let events = payload.get_events();
+    let events: Vec<crate::presentation::model::line_webhook::LineWebhookEvent> =
+        payload.get_events();
 
-    for event in events.iter() {
+    for event in events {
         let event_type = &event.r#type;
         match event_type {
             LineWebhookEventType::Follow => {
-                println!("event_type: {:?}", event_type);
+                let linebot_webhook_usecase =
+                    LinebotWebhookUseCase::new(Arc::new(LineUserAuthRepository::new()));
+                linebot_webhook_usecase.create_user(event.into()).await;
             }
             LineWebhookEventType::Unfollow => {
                 println!("event_type: {:?}", event_type);
