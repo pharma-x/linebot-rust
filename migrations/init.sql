cdb721f3fc28e8327b4ec0877f482b95080d87ce
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-17 10:16:27 +0900

    テーブル作成のSQLを修正した&indexを作成した

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,20 +1,29 @@
 CREATE TABLE primary_users (
   id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4;
 
-CREATE TABLE line_auth (
-  line_id VARCHAR(32) NOT NULL PRIMARY KEY, -- ユーザーIDの値は、U[0-9a-f]{32}の正規表現にマッチする文字列
+-- line_id: ユーザーIDの値は、U[0-9a-f]{32}の正規表現にマッチする文字列
+-- display_name: displayNameの制限の記述は見つけられなかった（https://developers.line.biz/ja/reference/messaging-api/#get-profile）
+-- picture_url: 一般的なURL長内に収まる想定
+CREATE TABLE line_users (
+  line_id VARCHAR(32) NOT NULL PRIMARY KEY,
   primary_user_id BIGINT NOT NULL,
-  FOREIGN KEY(primary_user_id) REFERENCES primary_users(id) NOT NULL,
-  display_name VARCHAR(255) NOT NULL DEFAULT '', -- displayNameの制限の記述は見つけられなかった（https://developers.line.biz/ja/reference/messaging-api/#get-profile）
-  picture_url VARCHAR(2048) NOT NULL DEFAULT '', -- 一般的なURL長内に収まる想定
+  FOREIGN KEY(primary_user_id) REFERENCES primary_users(id),
+  display_name VARCHAR(255) NOT NULL DEFAULT '',
+  picture_url VARCHAR(2048) NOT NULL DEFAULT '',
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
+CREATE INDEX idx_line_users_primary_user_id ON line_users(primary_user_id);
+
+-- document_id: UUID v4 を使っても36桁
 CREATE TABLE talk_rooms (
-  id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  document_id VARCHAR(36) NOT NULL UNIQUE, -- uuid v4を使っても36桁
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  document_id VARCHAR(36) NOT NULL PRIMARY KEY,
+  primary_user_id BIGINT NOT NULL,
+  FOREIGN KEY(primary_user_id) REFERENCES primary_users(id),
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4;
+
+CREATE INDEX idx_talk_rooms_primary_user_id ON talk_rooms(primary_user_id);
