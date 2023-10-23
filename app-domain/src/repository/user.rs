35a786c3583e42d4e15bbd903a8132a876b916e2
Akihiro Ueno <a.ueno@pharma-x.co.jp>
2023-10-23 13:08:59 +0900

    fmtを整えた

--- app-domain/src/repository/user.rs
+++ app-domain/src/repository/user.rs
@@ -10,8 +10,6 @@ use async_trait::async_trait;
 pub trait UserRepository {
     async fn get_user(&self, source: AuthUserId) -> anyhow::Result<User>;
     async fn get_line_user(&self, source: LineId) -> anyhow::Result<User>;
-
     async fn create_user(&self, source: UserProfile) -> anyhow::Result<User>;
     async fn create_line_user(&self, source: LineUserProfile) -> anyhow::Result<User>;
 }
-
