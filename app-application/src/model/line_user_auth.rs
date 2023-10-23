bab367f60b78f643582260b3bb250448f464de13
Akihiro Ueno <a.ueno@pharma-x.co.jp>
2023-10-23 17:30:51 +0900

    lineのuser profileを取得するためのaccess tokenを環境変数から取得するようにした

--- app-application/src/model/line_user_auth.rs
+++ app-application/src/model/line_user_auth.rs
@@ -17,8 +17,8 @@ impl From<CreateLineUserAuth> for AuthUserId {
 impl TryFrom<CreateLineUserAuth> for UserAuthData {
     type Error = anyhow::Error;
     fn try_from(c: CreateLineUserAuth) -> anyhow::Result<Self> {
-        // todo: 環境変数からlineのaccess tokenを取得する
-        let auth_token = "token".to_string();
+        let auth_token = env::var("LINE_ACCESS_TOKEN")
+            .unwrap_or_else(|_| panic!("LINE_ACCESS_TOKEN is not set"));
         Ok(UserAuthData::Line(LineUserAuthData {
             auth_id: AuthUserId::Line(LineId::new(c.user_id)),
             auth_token: AuthToken::Line(LineAuthToken::new(auth_token)),
